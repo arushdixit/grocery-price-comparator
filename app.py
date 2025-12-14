0990bde231771e2e93442bb258f08b7c58784198
arushdixit <dixit.arush@gmail.com>
2025-12-14 09:53:46 +0400

    feat: Update app run configuration on local network

--- app.py
+++ app.py
@@ -607,4 +607,4 @@ if __name__ == '__main__':
         # Reloader subprocess - start preloading
         threading.Thread(target=preload_browsers, daemon=True).start()
     
-    app.run(debug=True, host='127.0.0.1', port=5000)
+    app.run(host='0.0.0.0', port=9000)
